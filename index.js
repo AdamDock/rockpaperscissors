7d17475f210519628b184a63e72b5f63e192e325
Adam Dockstader <39977252+AdamDock@users.noreply.github.com>
2023-08-22 06:40:00 -0700

    Add DOM buttons for selection.Added score interfac
    Used DOM manipulation to add buttons and text for simple play of the
    game. Changed game to quit once one player reaches 5.

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
+let userWin = 0;
+let cpuWin = 0;
+let tie = 0;
+
+const rock = document.querySelector('.rockButton');
+const paper = document.querySelector('.paperButton');
+const scissors = document.querySelector('.scissorButton');
+const score = document.querySelector('.score');
+const result = document.querySelector('.result');
+
+rock.addEventListener('click', ()=>{
+    game("rock")
+}
+)
+paper.addEventListener('click', ()=>{
+    game("paper")
+}
+)
+scissors.addEventListener('click', ()=>{
+    game("scissors")
+}
+)
 function getComputerChoice() {
     let choice ="";
     let rand = Math.floor(Math.random() * (4-1)+1);
@@ -17,17 +39,14 @@ function getComputerChoice() {
     }
     return choice;
 }
-function getUserChoice(){
-    let user = prompt("Choose rock, paper, or scissors")
-    return user;
-}
 
-
-function playRockPaperScissors(){
+//Create buttons for each option and add event listener to start playRockpaperscissors function with player selection
+function playRockPaperScissors(user){
     let computer = getComputerChoice();
-    let user = getUserChoice();
-    if(user===computer)
+    if(user===computer){
+        tie++;
         return "It is a tie";
+    }
     else if(computer==="rock"){
         return computerChoseRock(user);
     }
@@ -40,31 +59,41 @@ function playRockPaperScissors(){
 }
 function computerChoseRock(userChoice){
     if(userChoice==="paper"){
+        userWin++;
         return "Computer chose rock. Paper wraps rock. You wins.";
     }
     else if(userChoice==="scissors"){
+        cpuWin++;
         return "Computer chose rock. Rock smashes scissors. Computer wins.";
     }
 }
 function computerChosePaper(userChoice){
     if(userChoice==="rock"){
+        cpuWin++;
         return "Computer chose paper. Paper wraps rock. Computer wins";
     }
     else if(userChoice==="scissors"){
+        userWin++;  
         return "Computer chose paper. Scissors cut paper. You win!";
     }
 }
 function computerChoseScissors(userChoice){
     if(userChoice==="paper"){
+        cpuWin++;
         return "Computer chose scissors. Scissors cut paper. Computer wins";
     }
     else if(userChoice==="rock"){
+        userWin++;
         return "Computer chose scissors. Rock smashes scissors. You win!";
     }
 }
-let game = ()=>{
-    for(let i=0; i<5;i++){
-        console.log(playRockPaperScissors());
+let game = (user)=>{
+    if(userWin < 5 && cpuWin < 5){
+    result.textContent= playRockPaperScissors(user);
+    score.textContent = "Score is "+ userWin+ " to "+cpuWin+ " with "+ tie+" ties";
+    }
+    else{
+        result.textContent = "Game Over";
     }
 }
 game();
